5622955834ad91c1ba4d491a65ba375e08f41cf2
Arsha <Arsha@NOOR>
2024-08-13 12:41:48 +0400

    Add project files.

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BulkyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db ?? throw new ArgumentNullException(nameof(db), "Database context is null");
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
